9de006b58c94aecbc6013a0f26adf9005387c126
Caelin <caelinsutch@gmail.com>
2020-04-18 11:08:29 -0700

    Updated launch file with a more comprehensive example

--- launch/composition_launch.py
+++ launch/composition_launch.py
@@ -6,11 +6,25 @@ Limitations of this container:
 """
 
 import launch
+import os
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
+from ament_index_python.packages import get_package_share_directory
+
+# Set your camera namespace
+NAMESPACE = "/camera"
+# GSCAM config (replace with yours
+gscamConfig = 'videotestsrc ! video/x-raw, format=BGRx ! videoconvert'
+# Location of config directory (for yamls)
+config_dir = os.path.join(get_package_share_directory('your-package'), 'config')
+# Get the location of your params file (formatted same as ROS1
+paramsFile = os.path.join(config_dir, "params.yaml")
+# Get the location of your camera calibration file
+cameraConfig = 'file://' + os.path.join(config_dir, "ost.yaml")
 
 
 def generate_launch_description():
+
 
     container = ComposableNodeContainer(
         node_name='my_container',
@@ -30,8 +44,29 @@ def generate_launch_description():
                 node_name='image_subscriber',
                 extra_arguments=[{'use_intra_process_comms': True}],
             ),
+            # Example of remapping and parameter input
+            ComposableNode(
+                package='gscam',
+                node_plugin='gscam::GSCamNode',
+                node_name='gscam',
+                node_namespace=NAMESPACE,
+                # Remap inputs to the correct namespace
+                remappings=[
+                    ('/image_raw', NAMESPACE + '/image_raw'),
+                    ('/camera_info', NAMESPACE + '/camera_info')
+                ],
+                parameters=[
+                    # Use a params file
+                    os.path.join(get_package_share_directory('br-perception'), "config", "params.yaml"),
+                    # Camera Name
+                    {'camera_name': 'right'},
+                    # Gscam config
+                    {'gscam_config': gscamConfig},
+                    # Info URL (Camera calibration information)
+                    {'camera_info_url': cameraConfig},
+                ]
+            )
         ],
         output='screen',
     )
-
     return launch.LaunchDescription([container])
